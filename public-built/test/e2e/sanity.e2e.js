2bab571aebd279cab8573b6cb6374feccb6e1c6d
Shayan Mohanty <shayan@evolucix.com>
2013-09-18 19:46:22 -0500

    Jk. Now it's removed

--- public-built/test/e2e/sanity.e2e.js
+++
@@ -1,6 +0,0 @@
-describe('Sample application', function() {
-  it('should display the home page', function() {
-    browser().navigateTo('/');
-    expect(element('title').text()).toBeDefined();
-  });
-});
